82d297097b80ea9b1800f3c4045dc664f526a3fb
ckymichael <35571104+ckymichael@users.noreply.github.com>
2019-04-05 01:59:44 +0800

    v1

---
+++ model/deep_walk.py
@@ -0,0 +1,121 @@
+import random
+import numpy as np
+import logging
+import networkx as nx
+from sklearn.metrics import roc_auc_score
+import pandas
+from tqdm import tqdm
+from deepwalk import graph
+from deepwalk import walks as serialized_walks
+from deepwalk import skipgram
+from gensim.models import Word2Vec
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+def get_neighbourhood_score(local_model, node1, node2):
+    try:
+        vector1 = local_model.wv.syn0[local_model.wv.index2word.index(node1)]
+        vector2 = local_model.wv.syn0[local_model.wv.index2word.index(node2)]
+        return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+    except:
+        return random.random()
+
+def get_AUC(model, true_edges, false_edges):
+    true_list = list()
+    prediction_list = list()
+    for edge in true_edges:
+        tmp_score = get_neighbourhood_score(model, str(edge[0]), str(edge[1]))
+        true_list.append(1)
+        prediction_list.append(tmp_score)
+
+    for edge in false_edges:
+        tmp_score = get_neighbourhood_score(model, str(edge[0]), str(edge[1]))
+        true_list.append(0)
+        prediction_list.append(tmp_score)
+    y_true = np.array(true_list)
+    y_scores = np.array(prediction_list)
+    return roc_auc_score(y_true, y_scores)
+
+# Start to load the train data
+
+train_edges = list()
+raw_train_data = pandas.read_csv('train.csv')
+for i, record in raw_train_data.iterrows():
+    train_edges.append((str(record['head']), str(record['tail'])))
+
+print('finish loading the train data.')
+
+# Start to load the valid/test data
+
+valid_positive_edges = list()
+valid_negative_edges = list()
+raw_valid_data = pandas.read_csv('valid.csv')
+for i, record in raw_valid_data.iterrows():
+    if record['label']:
+        valid_positive_edges.append((str(record['head']), str(record['tail'])))
+    else:
+        valid_negative_edges.append((str(record['head']), str(record['tail'])))
+
+print('finish loading the valid/test data.')
+
+
+number_walks = 10
+walk_length = 30
+dimension = 20
+window_size = 10
+workers = 10
+iterations = 10
+data_size = 535400
+max_memory_data_size = 10000
+
+# create empty adjacency lists - one for each node -
+# with a Python list comprehension
+
+def load_csv(file_, undirected=True):
+    G = graph.Graph()
+    raw_data = pandas.read_csv(file_)
+    for i, record in raw_data.iterrows():
+        G[str(record['head'])].append(str(record['tail']))
+        if undirected:
+             G[str(record['tail'])].append(str(record['head']))
+    G.make_consistent()
+    return G
+
+G = load_csv('train.csv')
+
+# write code to train the model here
+
+print("Number of nodes: {}".format(len(G.nodes())))
+
+num_walks = len(G.nodes()) * number_walks
+
+print("Number of walks: {}".format(num_walks))
+
+data_size = num_walks * walk_length
+
+print("Data size (walks*length): {}".format(data_size))
+
+
+print("Walking...")
+walks = graph.build_deepwalk_corpus(G, num_paths= number_walks,
+                                    path_length=walk_length, alpha=0, rand=random.Random(0))
+print("Training...")
+model = Word2Vec(walks, size=dimension, window=window_size, min_count=0, sg=1, hs=1,
+                workers=workers)
+
+resulted_embeddings = dict()
+for i, w in enumerate(model.wv.index2word):
+    resulted_embeddings[w] = model.wv.syn0[i]
+
+with open("test.txt", 'w') as f:
+    for key, value in resulted_embeddings.items():
+        f.write('%s:%s\n' % (key, value))
+
+tmp_AUC_score = get_AUC(model, valid_positive_edges, valid_negative_edges)
+
+
+print('tmp_accuracy:', tmp_AUC_score)
+print('end')
+
+
+
--- model/node.py
+++ model/node.py
@@ -1,13 +1,38 @@
+import ujson as json
+import node2vec
+import networkx as nx
+from gensim.models import Word2Vec
+import logging
 import random
 import numpy as np
 from sklearn.metrics import roc_auc_score
 import pandas
-from tqdm import tqdm
 
+def get_G_from_edges(edges):
+    edge_dict = dict()
+    # calculate the count for all the edges
+    for edge in edges:
+        edge_key = str(edge[0]) + '_' + str(edge[1])
+        if edge_key not in edge_dict:
+            edge_dict[edge_key] = 1
+        else:
+            edge_dict[edge_key] += 1
+    tmp_G = nx.DiGraph()
+    for edge_key in edge_dict:
+        weight = edge_dict[edge_key]
+        # add edges to the graph
+        tmp_G.add_edge(edge_key.split('_')[0], edge_key.split('_')[1])
+        # add weights for all the edges
+        tmp_G[edge_key.split('_')[0]][edge_key.split('_')[1]]['weight'] = weight
+    return tmp_G
 
 def get_neighbourhood_score(local_model, node1, node2):
-    # Provide the plausibility score for a pair of nodes based on your own model.
-
+    try:
+        vector1 = local_model.wv.syn0[local_model.wv.index2word.index(node1)]
+        vector2 = local_model.wv.syn0[local_model.wv.index2word.index(node2)]
+        return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+    except:
+        return random.random()
 
 def get_AUC(model, true_edges, false_edges):
     true_list = list()
@@ -25,13 +50,24 @@ def get_AUC(model, true_edges, false_edges):
     y_scores = np.array(prediction_list)
     return roc_auc_score(y_true, y_scores)
 
+EMBEDDING_FILENAME = '../data/test.csv'
 
+directed = True
+p = 1
+q = 1
+num_walks = 6
+walk_length = 9
+dimension = 230
+window_size = 10
+num_workers = 4
+iterations = 3
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# Start to load the train data
+# Start to load the raw network
 
 train_edges = list()
-raw_train_data = pandas.read_csv('train.csv')
+raw_train_data = pandas.read_csv('../data/train.csv')
 for i, record in raw_train_data.iterrows():
     train_edges.append((str(record['head']), str(record['tail'])))
 
@@ -41,7 +77,7 @@ print('finish loading the train data.')
 
 valid_positive_edges = list()
 valid_negative_edges = list()
-raw_valid_data = pandas.read_csv('valid.csv')
+raw_valid_data = pandas.read_csv('../data/valid.csv')
 for i, record in raw_valid_data.iterrows():
     if record['label']:
         valid_positive_edges.append((str(record['head']), str(record['tail'])))
@@ -50,13 +86,32 @@ for i, record in raw_valid_data.iterrows():
 
 print('finish loading the valid/test data.')
 
+train_edges = list(set(train_edges))
 
-# write code to train the model here
 
+train_nodes = list()
+for e in train_edges:
+    train_nodes.append(e[0])
+    train_nodes.append(e[1])
+train_nodes = list(set(train_nodes))
 
-# replace 'your_model' with your own model and use the provided evaluation code to evaluate.
-tmp_AUC_score = get_AUC(Your_model, valid_positive_edges, valid_negative_edges)
+# Create a node2vec object with training edges
+G = node2vec.Graph(get_G_from_edges(train_edges), directed, p, q)
+# Calculate the probability for the random walk process
+G.preprocess_transition_probs()
+# Conduct the random walk process
+walks = G.simulate_walks(num_walks, walk_length)
+# Train the node embeddings with gensim word2vec package
+model = Word2Vec(walks, size=dimension, window=window_size, min_count=0, sg=1, workers=num_workers, iter=iterations)
+# Save the resulted embeddings (you can use any format you like)
+resulted_embeddings = dict()
+for i, w in enumerate(model.wv.index2word):
+    resulted_embeddings[w] = model.wv.syn0[i]
+# Test the performance of resulted embeddings with a link prediction task.
+tmp_AUC_score = get_AUC(model, valid_positive_edges, valid_negative_edges)
 
 print('tmp_accuracy:', tmp_AUC_score)
+# Save embeddings for later use
+model.wv.save_word2vec_format(EMBEDDING_FILENAME)
 
 print('end')
